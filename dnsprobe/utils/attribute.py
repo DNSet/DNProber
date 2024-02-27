f3ab8a5fa645752b152eaf5ea5117ba5772c6aab
Mingzhe Zou <zoumingzhe@qq.com>
2024-02-27 18:51:03 +0800

    setup: attribute: fix __url_bugs__ error

--- dnsprobe/utils/attribute.py
+++ dnsprobe/utils/attribute.py
@@ -1,12 +1,11 @@
 # coding:utf-8
 
-import os
 from urllib.parse import urljoin
 
 __name__ = "dnsprobe"
 __version__ = "0.1.alpha.3"
 __description__ = "Domain Name System Probe."
-__url_home__ = "https://github.com/DNSet/DNSProber"
+__url_home__ = "https://github.com/DNSet/DNSProber/"
 __url_code__ = __url_home__
 __url_docs__ = __url_home__
 __url_bugs__ = urljoin(__url_home__, "issues")
